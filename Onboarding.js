2f40a2270fb66982ea8c0106a9192dabbef05b88
VritantaNextgen <VritantaNextgen@gmail.com>
2024-09-17 22:11:06 +0530

    Create Onboarding.js

---
+++ Onboarding.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Onboarding = () => {
+    return (
+        <section id="onboarding">
+            <h2>Onboarding</h2>
+            <p>This is the onboarding page.</p>
+        </section>
+    );
+};
+
+export default Onboarding;
